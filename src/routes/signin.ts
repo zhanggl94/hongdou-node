28544c0130cff4e9550969c18442cbb2a5fe3372
zhanggl94 <zhanggl94@163.com>
2021-07-09 17:51:40 +0800

    update: 修改signin时返回的参数及状态码

--- src/routes/signin.ts
+++ src/routes/signin.ts
@@ -1,3 +1,11 @@
+/*
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: zhanggl
+ * @Date: 2021-07-08 11:27:45
+ * @LastEditors: zhanggl
+ * @LastEditTime: 2021-07-09 15:09:00
+ */
 import express, { Request } from 'express';
 import mySqlOperate from '../db/mysqlOperate';
 import { cryPassword } from '../utils/cryptoUtil';
@@ -14,10 +22,10 @@ router.post('/signin', async (req, res) => {
         if (result.isOk) {
             res.send(result);
         } else {
-            res.status(400).send(result);
+            res.status(401).send(result);
         }
     } catch (error) {
-        res.status(400).send({ isOk: false, error, message: 'There has some system error.' });
+        res.status(401).send({ isOk: false, error, message: 'There has some system error.' });
     }
 })
 
@@ -39,6 +47,7 @@ const isUserExist = async (req: Request) => {
                 const updateSql = `UPDATE user SET lastlogintime = ? where id = ?`;
                 const updateParamList = [formatDateHour24(new Date(), constants.time_zone_zh_cn), id];
                 mySqlOperate.query(updateSql, updateParamList);
+                result.data = { id: id, username: req.body.username }
             } else {
                 result.isOk = false;
                 result.message = 'Username or password is not corrected.';
@@ -52,6 +61,7 @@ const isUserExist = async (req: Request) => {
         result.error = error;
         result.message = 'There has some errors when query user.';
     }
+    console.log('result', result)
     return result;
 }
 
