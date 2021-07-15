6d8df5235fc53fd75e517c66b5c05272b404526b
zhanggl94 <zhanggl94@163.com>
2021-07-15 17:55:40 +0800

    add: 添加修改用户密码的路由接口

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,26 +4,67 @@
  * @Autor: zhanggl
  * @Date: 2021-07-14 11:15:59
  * @LastEditors: zhanggl
- * @LastEditTime: 2021-07-14 11:19:35
+ * @LastEditTime: 2021-07-15 17:23:21
  */
 import express, { Request, Response } from 'express';
+import mySqlOperate from '../db/mysqlOperate';
 import ResponResult from '../module/ResponResult';
-import constants from '../utils/constants';
 import { cryPassword } from '../utils/cryptoUtil';
-import { formatDateHour24 } from '../utils/util';
 
 const router = express.Router();
 
-router.post('/edit', (req: Request, res: Response) => {
-  const result: ResponResult = new ResponResult(res.locals);
+// 更新用户信息
+router.post('/edit', async (req: Request, res: Response) => {
+  let result: ResponResult = new ResponResult(res.locals);
+  let resCode = 200;
   try {
-    const { id, username, password } = req.body;
-    const currentDateTime = formatDateHour24(new Date(), constants.time_zone_zh_cn);
-    const cryPwd = cryPassword(password, currentDateTime);
-    const sql = `select id from user where id=? AND username=? AND password=?`;
+    const { id, username, oldPassword, newPassword } = req.body;
+    const sql = `select username, password, createtime from user where id=? limit 1`;
+    const paramList: Array<string> = [id];
+    const data: any = await mySqlOperate.query(sql, paramList)
+    if (data.length) {
+      const dbUsername = data[0].username;
+      const dbPassword = data[0].password;
+      const dbCreatetime = data[0].createtime;
+      const cryPwd = cryPassword(oldPassword, dbCreatetime);
+      console.log(`oldPassword: ${oldPassword}, cryPwd: ${cryPwd}`)
+      console.log('dbPassword ', dbPassword)
+      if (cryPwd === dbPassword) {
+        const newCryPwd = cryPassword(newPassword, dbCreatetime);
+        const editParamList = [newCryPwd, id];
+        result = await editUser(res, editParamList)
+      } else {
+        result.isOk = false;
+        result.message = '原密码不正确';
+      }
+    }
   } catch (error) {
-
+    resCode = 400;
+    result.isOk = false;
+    result.error = error;
+    result.message = "Query user error.";
+  } finally {
+    res.status(resCode).send(result)
   }
 })
+
+// 更新用户信息
+const editUser = async (res: Response, paramList: Array<string>) => {
+  const result: ResponResult = new ResponResult(res.locals);
+  let resCode = 200;
+  const updateSql = `UPDATE user SET password = ? WHERE id = ?`
+  try {
+    const updateData = await mySqlOperate.query(updateSql, paramList)
+    if (!updateData.affectedRows) {
+      result.isOk = false;
+      result.message = 'Update user failed.';
+    }
+  } catch (error) {
+    resCode = 400;
+    result.error = error
+    result.message = 'Update user error.'
+  }
+  return result;
+}
 
 export default router;
